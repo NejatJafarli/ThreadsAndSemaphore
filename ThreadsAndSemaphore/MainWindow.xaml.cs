5e0dab0fb6d4c03d729e94eae792168b18a259c7
nicat ceferli <mega.ceferli@gmail.com>
2022-01-10 23:05:56 +0400

    Add project files.

---
+++ ThreadsAndSemaphore/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ThreadsAndSemaphore
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static int THREADCOUNT = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public ObservableCollection<string> FakeTheads { get; set; }=new ObservableCollection<string>();
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            THREADCOUNT++;
+            FakeTheads.Add($"Thread {THREADCOUNT}");
+        }
+    }
+}
